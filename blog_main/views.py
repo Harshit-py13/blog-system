eda3f1102447e39fbb12c99950c9e7a45571148d
Harshit <harshit.tiwari0413@gmail.com>
2025-12-16 23:15:31 +0530

    Social link and search features

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from abouts.models import About
 from blogs.models import Blog, Category
 
 def home(request):
@@ -6,8 +7,16 @@ def home(request):
     featured_posts = Blog.objects.filter(is_featured=True, status='Published').order_by('updated_at')
     posts = Blog.objects.filter(is_featured=False, status='Published')
 
+
+    # Fetch about us
+    try:
+        about = About.objects.get()
+    except:
+        about = None
+        
     context = {
         'featured_posts' : featured_posts,
-        'posts': posts
+        'posts' : posts,
+        'about' : about,
     }
     return render(request, 'home.html', context)
